2661b655d7af3402db5a93da753fcb5c74f34e4b
isiah-a <isiaha478@gmail.com>
2025-04-01 16:50:58 -0400

    Results of processing file was created.

--- WordForWord.py
+++ WordForWord.py
@@ -1,6 +1,5 @@
 import os
 import re
-from itertools import count
 
 
 class Word4word:
@@ -10,7 +9,7 @@ class Word4word:
             file_content = file.read()
         return file_content
 
-    def wc(obj: str):
+    def wc(self, obj):
         word_count = len(re.findall(r'\w+', obj))
         words = word_count
         chars = len(obj)
@@ -18,12 +17,18 @@ class Word4word:
         tupe = (lines, words, chars)
         return tupe
 
-    def word_freq(obj: str):
-        new_lst = obj.split()
-        freq = [new_lst.count(c) for c in new_lst]
-        return dict(zip(new_lst, freq))
+    def word_freq(self, obj):
+        onlywrds = re.sub(r'[^a-zA-Z0-9]', ' ', obj)
+        splitwrds = onlywrds.lower().split()
+        wrd = {}
+        for l in splitwrds:
+            if l in wrd:
+                wrd[l] += 1
+            else:
+                wrd[l] = 1
+        return wrd
 
-    def letter_freq(obj: str):
+    def letter_freq(self, obj):
         onlywrds = re.sub(r'[^a-zA-Z0-9]', ' ', obj).lower()
         freq = {}
         for l in onlywrds:
@@ -33,7 +38,7 @@ class Word4word:
                 freq[l] = 1
         return freq
 
-    def freq_word(obj: str, word, total_words):
+    def freq_word(self, obj, word, total_words):
         count = 0
         new_string = re.sub(r'[^\w\s]', '', obj)
         split_new_string = new_string.split()
@@ -45,12 +50,27 @@ class Word4word:
 
 
 
-x = Word4word()
-b = x.printfile('/Users/isiah/Projects/P1/PyWordForWord/testdata/testdata5a.txt')
-tupe = Word4word.wc(b)
-print(tupe)
+# x = Word4word()
+# b = x.printfile('/Users/isiah/Projects/P1/PyWordForWord/testdata/testdata5a.txt')
+# tupe = Word4word.wc(b)
+# print(tupe)
 # print(Word4word.word_freq(b))
-print(Word4word.letter_freq(b))
-print(Word4word.freq_word(b, 'the', tupe[1]))
+# print(Word4word.letter_freq(b))
+# print(Word4word.freq_word(b, 'the', tupe[1]))
+
+path = '/Users/isiah/Projects/P1/PyWordForWord/testdata'
+
+with open('ResultsOfProcessing.txt', 'w') as text_file:
+    for file in os.listdir(path):
+        full_path = "%s/%s" % (path, file)
+        wrd = Word4word()
+        b = wrd.printfile(full_path)
+        text_file.write(file + '\n')
+        text_file.write(str(wrd.wc(b)) + '\n')
+        text_file.write(str(wrd.word_freq(b))+ '\n')
+        text_file.write(str(wrd.letter_freq(b))+ '\n')
+        text_file.write('\n\n\n')
+
+
 
 
